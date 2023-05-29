e8cb745ab75119e20ccb631e50313e66bd8de0ab
datnt <datng58@@gmail.com>
2023-05-29 13:08:30 +0700

    textinput animation và scrollIndicator animation

--- src/screens/HeaderUtop/index.js
+++ src/screens/HeaderUtop/index.js
@@ -1,5 +1,5 @@
-import React, {useRef,useCallback, useEffect} from 'react';
-import {View, Text, SafeAreaView, TextInput, ScrollView,Button} from 'react-native';
+import React, { useRef, useCallback, useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, TextInput, ScrollView, Button } from 'react-native';
 import styles from './styles';
 import Animated, {
   useAnimatedStyle,
@@ -9,11 +9,12 @@ import Animated, {
   useAnimatedScrollHandler,
   interpolate,
 } from 'react-native-reanimated';
-import {ICONS} from '../../constants/icons';
-import {SCREEN_SIZE} from '../../constants/screenSize';
+import { ICONS } from '../../constants/icons';
+import { SCREEN_SIZE } from '../../constants/screenSize';
 import Crashes from 'appcenter-crashes';
 import Analytics from 'appcenter-analytics'
-  
+import { FlatList } from 'react-native-gesture-handler';
+
 const headerHeight = SCREEN_SIZE.HEIGHT * 0.3;
 const headerHeight0225 = SCREEN_SIZE.HEIGHT * 0.225;
 const headerHeightScroll = SCREEN_SIZE.HEIGHT * 0.15;
@@ -35,18 +36,31 @@ const inputRange5 = [-50, 0, spaceScroll / 2, spaceScroll, 150];
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView);
 
+
+
+const listData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+const ITEM_HEIGHT = SCREEN_SIZE.WIDTH * 0.1
+const ITEM_WIDTH = SCREEN_SIZE.WIDTH * 0.3
+const SCROLL_INDICATOR_WIDTH = 50
+
 const HeaderUtop = () => {
   const animatedValue = useSharedValue(0);
   const scrollViewRef = useRef(null);
 
+  const animatedValue2 = useSharedValue(0);
+
+  const [visibleBarWidth, setVisibleBarWidth] = useState(0)
+  const [BarWidth, setBarWidth] = useState(1)
+
   //--------------------------------------------
 
   const scrollViewHandler = event => {
-    console.log(
-      '*********Event********',
-      event?.nativeEvent?.contentOffset.y,
-      headerHeight - headerHeightScroll,
-    );
+    // console.log(
+    //   '*********Event********',
+    //   event?.nativeEvent?.contentOffset.y,
+    //   headerHeight - headerHeightScroll,
+    // );
     animatedValue.value = event?.nativeEvent?.contentOffset.y;
   };
 
@@ -55,7 +69,7 @@ const HeaderUtop = () => {
   const onScrollEndDrag = event => {
     //console.log('***********END************', event.nativeEvent);
     if (event.nativeEvent?.contentOffset.y < 150) {
-      scrollViewRef.current.scrollTo({y: 0, animated: true});
+      scrollViewRef.current.scrollTo({ y: 0, animated: true });
     }
   };
 
@@ -70,7 +84,7 @@ const HeaderUtop = () => {
     );
     return {
       opacity: opacity,
-      transform: [{translateY}],
+      transform: [{ translateY }],
     };
   });
 
@@ -183,7 +197,7 @@ const HeaderUtop = () => {
       [1, 1, 1.5, 1.5],
     );
     return {
-      transform: [{scale}],
+      transform: [{ scale }],
     };
   });
 
@@ -204,7 +218,7 @@ const HeaderUtop = () => {
     ]);
 
     return {
-      transform: [{scale}, {translateX}],
+      transform: [{ scale }, { translateX }],
     };
   });
 
@@ -234,26 +248,133 @@ const HeaderUtop = () => {
     ]);
 
     return {
-      transform: [{translateX}, {scale}, {translateY}],
+      transform: [{ translateX }, { scale }, { translateY }],
     };
   });
 
   //--------------------------------------------
 
-  const checkPreviousSession = useCallback(async ()=>{
+  const checkPreviousSession = useCallback(async () => {
     const didCrash = await Crashes.hasCrashedInLastSession();
 
-    if(didCrash){
+    if (didCrash) {
       const report = await Crashes.lastSessionCrashReport();
       alert("Xin lỗi về việc ứng dụng bị hỏng, chúng tôi đang cố gắng khắc phục !")
     }
 
-  },[])
+  }, [])
+
+  const [placeHolder, setPlaceHolder] = useState(['Sao chép tin nhắn', 'Ứng dụng tiện ích', 'Chúa tể animation'])
+  const [indexPlaceHolder, setIndexPlaceHolder] = useState(0)
+  const [indexContentPlaceHolder, setIndexContentPlaceHolder] = useState(0)
 
 
-  useEffect(()=>{
+
+  const changeIndex = () => {
+    let i = 0
+    let isOx = true
+    let indextemp = 0
+
+    setInterval(() => {
+      if (i == placeHolder?.[indexContentPlaceHolder]?.length) {
+        setTimeout(() => {
+          isOx = false
+        }, 500)
+      }
+
+      if (i == -1) {
+
+        console.log('*************', indextemp)
+        if (indextemp + 1 < placeHolder?.length) {
+          indextemp++
+
+        } else {
+          indextemp = 0
+        }
+
+        setIndexContentPlaceHolder(indextemp)
+        isOx = true
+
+      }
+
+      if (isOx) {
+        if (i < placeHolder?.[indexContentPlaceHolder]?.length)
+          i++
+
+      }
+      else {
+        if (i >= 0)
+          i--
+      }
+      setIndexPlaceHolder(i)
+    }, 80)
+
+
+
+
+  }
+
+  useEffect(() => {
     checkPreviousSession()
-  },[])
+    changeIndex()
+
+  }, [])
+
+
+  const scrollViewHandler2 = event => {
+    console.log(
+      '*********Event********',
+      event?.nativeEvent?.contentOffset.x
+
+    );
+    animatedValue2.value = event?.nativeEvent?.contentOffset.x;
+  };
+
+  const scrollIndicatorStyle = useAnimatedStyle(() => {
+
+    const translateX = interpolate(animatedValue2.value, [0, listData?.length * ITEM_WIDTH], [0, SCROLL_INDICATOR_WIDTH]);
+
+    const borderLeft = interpolate(animatedValue2.value, [0, 50, (listData?.length * ITEM_WIDTH) - 50, listData?.length * ITEM_WIDTH], [20, 0, 0, 20]);
+    const borderRight = interpolate(animatedValue2.value, [0, 50, (listData?.length * ITEM_WIDTH) / 2, (listData?.length * ITEM_WIDTH) - 50, listData?.length * ITEM_WIDTH], [0, 0, 0, 10, 20]);
+
+
+    return {
+      borderBottomLeftRadius: borderLeft,
+      borderTopLeftRadius: borderLeft,
+      borderBottomRightRadius: borderRight,
+      borderTopRightRadius: borderRight,
+      transform: [{ translateX }]
+    }
+  });
+
+
+
+
+
+  const renderItem = ({ item, index }) => {
+    let rgb = `rgb(${index * 10},${index * 20},${index * 10})`
+    return (
+      <View style={{ height: ITEM_HEIGHT, width: ITEM_WIDTH, backgroundColor: rgb }} />
+    )
+  }
+
+  const renderShowScrollIndicator = (size = 1, width = 50, height = 10, container_width = SCREEN_SIZE.WIDTH) => {
+
+    let item_W = (visibleBarWidth / (BarWidth)) * width;
+
+    return (
+      <View style={{
+        width: width, height: height, backgroundColor: 'red', marginVertical: 5, borderRadius: height,
+        alignSelf: 'center'
+      }} >
+        <Animated.View style={[{
+          position: 'absolute', width: item_W, backgroundColor: 'blue', height: height,
+        }, scrollIndicatorStyle]} />
+      </View>
+    )
+
+  }
+
 
   return (
     <View style={styles().container}>
@@ -282,7 +403,8 @@ const HeaderUtop = () => {
               style={[styles('', 2).searchContainer, searchContainer]}>
               <AnimatedTextInput
                 style={[styles().searchInput]}
-                placeholder={'Search'}
+                placeholder={indexPlaceHolder >= 0 ? placeHolder[indexContentPlaceHolder]?.slice(0, indexPlaceHolder) : ''}
+                placeholderTextColor='black'
               />
             </Animated.View>
           </Animated.View>
@@ -371,8 +493,39 @@ const HeaderUtop = () => {
           <Text>abc</Text>
           <Text>abc</Text>
           <Text>abc</Text>
-          <Button title='Test Crash App' onPress = {()=>Crashes.generateTestCrash()} />
-           <Button title='Analytic track event' onPress = {()=>Analytics.trackEvent('track_event')} />
+          <Button title='Test Crash App' onPress={() => Crashes.generateTestCrash()} />
+          <Button title='Analytic track event' onPress={() => Analytics.trackEvent('track_event')} />
+
+          <View style={{}}>
+            <FlatList data={listData}
+              horizontal={true}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={renderItem}
+              showsHorizontalScrollIndicator={false}
+              scrollEventThrottle={16}
+              onScroll={scrollViewHandler2}
+              bounces={false}
+              onLayout={({
+                nativeEvent: {
+                  layout: { width }
+                }
+              }) => {
+                console.log('VisibleScrollBarWidth : ', visibleBarWidth)
+                setVisibleBarWidth(width);
+              }}
+              onContentSizeChange={width => {
+                console.log('CompleteScrollBarWidth : ', BarWidth)
+                setBarWidth(width);
+              }}
+
+            />
+
+            {renderShowScrollIndicator(listData?.length)}
+          </View>
+
+
+
+
         </View>
       </ScrollView>
     </View>
